663df03ff152a575f90f32734213958eb7479df1
PriyanshGupta2002 <guptapriyansh23@gmail.com>
2024-11-10 21:24:32 +0530

    fixes in env

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import BannerCarousel from "@/components/banner-carousel";
 import EzySteps from "@/components/ezy-steps";
 import ImageBanner from "@/components/image-banner";
-import TopCategories from "@/components/top-categories";
 import TopDeals from "@/components/top-deals";
 import React from "react";
 
